0ee3aeeb9936141ae4c79e1433a21c9e0d23e5f7
RicardoM99160 <inuriqui_cool2@outlook.com>
2019-12-15 00:19:16 -0600

    Firebase fallando

--- src/routes/add-consulta.js
+++ src/routes/add-consulta.js
@@ -1,15 +1,6 @@
 
-function addConsulta(database,idPaciente){
+function addConsulta(database,idPaciente,consulta){
     var idConsulta = "C00001";
-    var consulta = {
-        centroMedico: "Hospital Nacional de San Vicente",
-        diagnostico: "Cancer terminal",
-        doctor: "Alberto Peluca",
-        examenes: "Examen de tumor",
-        fechaConsulta: "14/12/2019",
-        incapacidad: "de por vida, se murio",
-        medicamentosSuministrados: "Parecetamol/Peluca" 
-    }
     database.ref("expedientes/"+idPaciente+"/consultas").child(idConsulta).set(consulta);
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -18,28 +18,61 @@ router.get("/", (req, res) => {
     res.render("index");
 });
 
+var prueba;
+
 router.post("/loggin",(req,res)=>{
     var bandera = false;
     var user = {
         username: req.body.username,
         password: req.body.password
     }
+    const empleados = db.ref().child("empleados");
+    console.log("AAAAAAAAAAAA",empleados);
     db.ref("empleados").once("child_added", (snapshot)=>{
+        prueba = snapshot.val();
+        console.log("Adentro",prueba);
         if(user.username == snapshot.key && user.password == snapshot.val().contra){
-            res.redirect("/buscar-paciente");
+            res.render("buscar-paciente");
         }else{
             bandera = true;
             res.render("index",{autenticado: bandera});
         }
     });
+
+    console.log("Afuera",prueba);
 });
 
-router.get("/buscar-paciente", (req,res) => {
-    res.render("buscar-paciente");
+router.post("/registrar-consulta", (req,res) => {
+    var idPaciente = req.body.idPaciente;
+    var consulta = {
+        centroMedico: req.body.centroMedico,
+        diagnostico: req.body.diagnostico,
+        doctor: req.body.doctor,
+        examenes: req.body.examenes,
+        fechaConsulta: req.body.fechaConsulta,
+        incapacidad: req.body.incapacidad,
+        medicamentosSuministrados: req.body.medicamento 
+    }
+    if(agregarConsulta(db,idPaciente,consulta))
+        res.render("buscar-paciente");
 });
 
-router.get("/delete-contact/:id", (req, res) => {
-    res.redirect("/");
+router.get("/formulario-consulta", (req,res) => {
+    res.render("consulta");
 });
 
+router.post("/buscar-paciente", (req,res) => {
+    var datos = {
+        idPaciente: req.body.idPaciente,
+        nombre: req.body.nombre
+    };
+    console.log("datos",datos);
+    db.ref("expedientes").once("value", (snapshot)=>{
+        console.log(snapshot.key);
+        if(datos.idPaciente == snapshot.key || datos.nombre == snapshot.val().nombre){
+            const paciente = snapshot.val();
+            res.render("consulta", {datos: paciente});
+        }
+    });
+});
 module.exports = router;
